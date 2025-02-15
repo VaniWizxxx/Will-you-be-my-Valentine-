bdde4e9bc367411ee525601f9b2d989430931d24
VaniWizxxx <nextthanku46@gmail.com>
2025-02-14 19:25:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -56,12 +56,12 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Positive?",
+    "Please :(...",
+    "Pleeeeaaaaasssseee",
+    "If you say no, I will be very sad",
+    "I'll do anything for you ❤️",
+    "But you're soo fineee 🫶🏻",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
